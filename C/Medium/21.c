2259abd5088e4f377b2ba9087b892c279670f826
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 12:54:44 +0530

    21 c

--- C/Medium/21.c
+++ C/Medium/21.c
@@ -1,6 +1,9 @@
 // 30) Prime Number Check Using a While Loop 
 
 
+// 30) Prime Number Check Using a While Loop 
+
+
 #include <stdio.h>
 
 int isPrime(int n) {
@@ -14,8 +17,10 @@ int isPrime(int n) {
 }
 
 int main() {
-    int num = 29;
-    if (isPrime(num))
+    int num = 6;
+    if(num==1)
+        printf("neither prime nor composite");
+    else if(isPrime(num))
         printf("%d is prime\n", num);
     else
         printf("%d is not prime\n", num);

d74702bd46894a752c87c08c02568d7ca9141349
Maanvi Shadakshari <siddhenpise10@gmail.com>
2024-10-05 13:52:11 +0530

    Update 53.py
    Fixed the indentation. Since we are dividing zero by an empty list we will get an exception

--- Python/Medium/53.py
+++ Python/Medium/53.py
@@ -1,13 +1,14 @@
- def calculate_average(lst):
- total = 0
- for num in lst:
- total += num
- return total / len(lst)
- def main():
- numbers = []
- try:
- avg = calculate_average(numbers)
- print(f"Average: {avg}")
-except ZeroDivisionError as e:
- print(f"ZeroDivisionError: {e}")
- main()
+def calculate_average(lst):
+  total = 0
+  for num in lst:
+   total += num
+  return total / len(lst)
+def main():
+  numbers = []
+  try:
+    avg = calculate_average(numbers)
+    print(f"Average: {avg}")
+  except ZeroDivisionError as e:
+    print(f"ZeroDivisionError: {e}")
+main()
+# since we are dividing zero by lst whoch here is an empty list we will get an exception 

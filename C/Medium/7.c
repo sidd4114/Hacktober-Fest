a08eb28aea7f613e701d99531f06de4ae2771b4a
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 12:42:15 +0530

    7 c

--- C/Medium/7.c
+++ C/Medium/7.c
@@ -14,7 +14,9 @@ int main() {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             printf("%d ", j);
+    
         }
+        printf("\n");
     }
     
     return 0;

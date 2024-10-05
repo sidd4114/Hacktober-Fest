2671ecbc51f4cd08327a70743264b53ed4a5bdb2
Niya Johnson <siddhenpise10@gmail.com>
2024-10-05 13:58:31 +0530

    updated79_easy_py
    added colons and parenthesis

--- Python/Easy/79.py
+++ Python/Easy/79.py
@@ -1,9 +1,9 @@
-def calculate_sum(a, b
+def calculate_sum(a, b):
     result = a + b
-    if result > 10
-        print("Result is greater than 10"
-    else
-        print("Result is 10 or less"
+    if result > 10:
+        print("Result is greater than 10")
+    else:
+        print("Result is 10 or less")
     return result
 
-sum_value = calculate_sum(5, 7
+sum_value = calculate_sum(5, 7)

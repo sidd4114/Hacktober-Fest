f920a3abea12a3a0318df090be6a732f2f177136
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:57:28 +0530

    28 c

--- C/Easy/28.c
+++ C/Easy/28.c
@@ -3,13 +3,14 @@
 int main() {
     int numbers[5];
     int i;
-    
+    printf("enter array elemtns\n");
     for (i = 0; i < 5; i++) {
-        scanf("%d", &numbers[i])
+        scanf("%d", &numbers[i]);
     }
     
     printf("The array elements are:\n");
+    for(i=0;i<5;i++)
+        printf("%d\n",numbers[i]);
     
-    return 0;
+    return 0;
 }
-

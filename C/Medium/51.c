8e3cfb212324e71d1d0b9c8b38edcc548f5a0b29
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:19:40 +0530

    51 c

--- C/Medium/51.c
+++ C/Medium/51.c
@@ -1,10 +1,10 @@
 
 #include <stdio.h>
-void myFunction(int a);
+void myFunction(float a);
 int main() {
     myFunction(5.5);
     return 0;
 }
-void myFunction(int a) {
-    printf("%d\n", a);
+void myFunction(float a) {
+    printf("%f\n", a);
 }

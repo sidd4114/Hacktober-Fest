170b0a1768f018b50db45b299311b7ab816dd9f6
Maanvi Shadakshari <siddhenpise10@gmail.com>
2024-10-05 12:56:21 +0530

    17easy python
    used f strings and corrected the use of inverted commas.

--- Python/Easy/17.py
+++ Python/Easy/17.py
@@ -1,11 +1,10 @@
-a=int(input(‘enter a num’))
-b=int(input(‘enter another number’))
-Sum=a+b
-print(‘Sum=’,sum)
+a=int(input("enter a num"))
+b=int(input("enter another number"))
+sum=a+b
+print(f"Sum:{sum}")
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
-Sum = a + b
 print("Sum: ", Sum)  # Incorrect print syntax for variables
 
 

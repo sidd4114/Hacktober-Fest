b9818ba16640a84e57692a20be720e4fe050a941
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:02:42 +0530

    24 c

--- C/Medium/24.c
+++ C/Medium/24.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main() {
-    int arr[5];
-    printf("%d", arr[5]);
+    int arr[5]={1,2,3,4,5};
+    int i;
+    for(i=0;i<5;i++)
+        printf("%d\n",arr[i]);
     return 0;
 }

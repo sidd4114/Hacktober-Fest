60fd0c7a2e0467006870fcef7dd541a69416c653
Maanvi Shadakshari <siddhenpise10@gmail.com>
2024-10-05 13:56:16 +0530

    Update 57.py
    Added else block, fixed indentation error and changed input form -5 to 5

--- Python/Medium/57.py
+++ Python/Medium/57.py
@@ -1,11 +1,13 @@
 def recursive_sum(n):
  if n == 0:
- return 0
- return n + recursive_sum(n - 1)
- def main():
- try:
- result = recursive_sum(-5)
- print(f"Sum: {result}")
- except RecursionError as e:
- print(f"RecursionError: {e}")
- main()
+  return 0
+ else:
+  return n + recursive_sum(n - 1)
+def main():
+  try:
+   result = recursive_sum(5)
+   print(f"Sum: {result}")
+  except RecursionError as e:
+   print(f"RecursionError: {e}")
+main()
+# since inpput is -5, we will get an error. changing it to 5.

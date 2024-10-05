725a9f63ae1678f9123ba4a99089684c24ed805b
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:55:39 +0530

    27 c

--- C/Easy/27.c
+++ C/Easy/27.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int i, j
+    int i, j;
     for (i = 1; i <= 5; i++) {
         for (j = 1; j <= i; j++) {
             printf("* ");
         }
         printf("\n");
     }
-    return 0;
+    return 0;
 }

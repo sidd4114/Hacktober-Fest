b0d2059fa44de6d2ccad066f21a5c45d3c9f5ee8
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:13:13 +0530

    33 c

--- C/Medium/33.c
+++ C/Medium/33.c
@@ -5,17 +5,18 @@
 // 1 2 3 4
 // 1 2 3 4 5
 
+
 #include <stdio.h>
 
 int main() {
     int n = 5;
     
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {  
+        for (int j = 1; j <= i; j++) {  
             printf("%d ", j);
         }
         printf("\n");
     }
 
-    return 0;
+    return 0;
 }

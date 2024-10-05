0d7a5ae9d0a72e94bc89775dec939a6017c44cac
Maanvi Shadakshari <siddhenpise10@gmail.com>
2024-10-05 13:38:20 +0530

    python medium 51
    Fixed the try catch block and added else statement. since input is -5, it will give exception error

--- Python/Medium/51.py
+++ Python/Medium/51.py
@@ -1,11 +1,14 @@
 def factorial(n):
  if n == 0:
- return 1
- return n * factorial(n - 1)
- def main():
+  return 1
+ else:
+  return n * factorial(n - 1)
+def main():
+#  since the input is -5, we will get an exception here 
  try:
- result = factorial(-5)
- print(f"Factorial: {result}")
+  result = factorial(-5)
+  print(f"Factorial: {result}")
  except RecursionError as e:
- print(f"RecursionError: {e}")
- main()
+  print(f"RecursionError: {e}")
+ finally: print(result)
+main()

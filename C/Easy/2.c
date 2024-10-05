a207a9c12dba83e5b8c421ed8a7357bff54a82ca
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:30:36 +0530

    2 c

--- C/Easy/2.c
+++ C/Easy/2.c
@@ -1,7 +1,7 @@
 int main() {
     int a = 10;
     double b = 5.5;
-    int c = a + b;
-    printf("The sum of a and b is %d", c);
-    return 0;
-} 
+    float c = a + b;
+    printf("The sum of a and b is %f", c);
+    return 0;
+}

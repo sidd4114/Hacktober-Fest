2a578df3621fea9f6ec61b950e50904a56236b91
Maanvi Shadakshari <siddhenpise10@gmail.com>
2024-10-05 12:52:34 +0530

    Medium 26 Python
    Fixed the code structure, added arguments to functions, corrected the spelling of return and called the functions with arguments

--- Python/Medium/26.py
+++ Python/Medium/26.py
@@ -1,10 +1,8 @@
-def multiply:
-    Return a*b
-a=int(input(‘enter a num’))
-b=int(input(‘enter another number’))
-
+def multiply(a,b):
+ return a*b
 def square(num):
     return num * num
-
-
-square  
+a=int(input("enter a num"))
+b=int(input("enter another number"))
+square(2) 
+multiply(3,4)

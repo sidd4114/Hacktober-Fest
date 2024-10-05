5c36c0335e8673a710c26784ee55065e65faa2a9
ERROR404 <siddhenpise10@gmail.com>
2024-10-05 13:32:51 +0530

    4 c

--- C/Easy/4.c
+++ C/Easy/4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
     int arr[5] = {1, 2, 3, 4};
-    printf("The first element of the array is %d", arr[5]);
-    return 0;
+    printf("The first element of the array is %d", arr[0]);
+    return 0;
 }

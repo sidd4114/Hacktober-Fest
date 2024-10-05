02e892fdf768452be5e1c4d67b5ff180e8089157
Maanvi Shadakshari <siddhenpise10@gmail.com>
2024-10-05 13:30:58 +0530

    Medium 44
    added for loops

--- Python/Medium/44.py
+++ Python/Medium/44.py
@@ -1,14 +1,13 @@
-def calculate_sum():
-    sum = 0
-    
-    
-    numbers = [1, 2, 3, 4]
-    total = sum(numbers)
-    print("The sum of the numbers is:", total)
-    
-    
-    more_numbers = [10, 20, 30]
-    result = sum(more_numbers)
-    print("The result is:", result)
 
+def calculate_sum():
+    sum=0
+    numbers = [1, 2, 3, 4]
+    for x in range (len(numbers)):
+        sum+=numbers[x]
+    print("The sum of the numbers is:",sum)
+    more_numbers = [10, 20, 30]
+    for y in range (len(more_numbers)):
+     sum+=more_numbers[y]
+    result =sum
+    print("The result is:", result)
 calculate_sum()
